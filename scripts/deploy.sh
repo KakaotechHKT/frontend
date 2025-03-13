f8f72177a718c69037900d207ca7ac4d78d49a0d
flydog98 <guinnessoverflow@gmail.com>
2025-03-13 13:06:55 +0900

    CI/CD: dev-prod 한 번에 사용하도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,36 +2,20 @@
 set -e
 
 echo "🚀 Deployment started..."
+echo "🔍 AWS_S3_BUCKET: $AWS_S3_BUCKET"
+echo "🔍 AWS_CLOUDFRONT_DISTRIBUTION_ID: $AWS_CLOUDFRONT_DISTRIBUTION_ID"
 
 BUILD_DIR="out"
 
-# 빌드 결과물 디렉터리 확인
 if [ ! -d "$BUILD_DIR" ]; then
   echo "❌ Build directory '$BUILD_DIR' does not exist. Aborting deployment."
   exit 1
 fi
 
-# 현재 브랜치 확인
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "🔎 Current branch: $BRANCH"
+echo "📤 Uploading files from $BUILD_DIR to S3 bucket: $AWS_S3_BUCKET"
+aws s3 sync "$BUILD_DIR" "s3://${AWS_S3_BUCKET}" --exclude ".DS_Store"
 
-# 환경 변수 설정
-if [ "$BRANCH" == "dev-test" ]; then
-  TARGET_S3_BUCKET="${AWS_DEV_S3_BUCKET}"
-  TARGET_CLOUDFRONT_ID="${AWS_DEV_CLOUDFRONT_DISTRIBUTION_ID}"
-  echo "🌱 Deploying to Dev environment..."
-else
-  TARGET_S3_BUCKET="${AWS_S3_BUCKET}"
-  TARGET_CLOUDFRONT_ID="${AWS_CLOUDFRONT_DISTRIBUTION_ID}"
-  echo "🚀 Deploying to Prod environment..."
-fi
-
-# S3에 정적 파일 업로드 (.DS_Store 파일 제외)
-echo "📤 Uploading files from $BUILD_DIR to S3 bucket: $TARGET_S3_BUCKET"
-aws s3 sync "$BUILD_DIR" "s3://${TARGET_S3_BUCKET}" --exclude ".DS_Store"
-
-# CloudFront 캐시 무효화
-echo "🌀 Invalidating CloudFront distribution: $TARGET_CLOUDFRONT_ID"
-aws cloudfront create-invalidation --distribution-id "$TARGET_CLOUDFRONT_ID" --paths "/*"
+echo "🌀 Invalidating CloudFront distribution: $AWS_CLOUDFRONT_DISTRIBUTION_ID"
+aws cloudfront create-invalidation --distribution-id "$AWS_CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"
 
 echo "✅ Deployment finished!"
