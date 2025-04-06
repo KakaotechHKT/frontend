cb7a7a6fdca89548c078f98fea15b5bd98bbd966
jihostudy <cale.jiho@gmail.com>
2025-04-06 21:51:51 +0900

    feat: 기존 밥팟 검색시 카테고리 > 검색바로 변경

--- src/app/(headerless)/part/page.tsx
+++ src/app/(headerless)/part/page.tsx
@@ -7,13 +7,14 @@ import KakaoMap from '@components/common/KakaoMap'
 import Chatroom from '@components/part/ChatRoom'
 import PlaceList from '@components/part/PlaceList'
 import RefuseModal from '@components/part/RefuseModal'
+import { Input } from '@components/ui/input'
 import { URL } from '@lib/constants/routes'
 import { ChattingType, CreateChatType } from '@lib/HTTP/API/chat'
 import { useMutationStore } from '@lib/HTTP/tanstack-query'
+import LucideIcon from '@lib/provider/LucideIcon'
 import { GeoType } from '@lib/types/part/part'
-import { cn } from '@lib/utils/utils'
 import { KTB_Position } from '@public/data'
-import { MainCategories, MainCategoriesType } from '@public/data/categories'
+import { MainCategoriesType } from '@public/data/categories'
 import { Chatting, ChatType } from '@public/data/ChatResponse'
 import { placeListDummyData } from '@public/data/restaurant'
 import LogoImage from '@public/images/logo.svg'
@@ -54,6 +55,8 @@ const PartPage = (): ReactNode => {
   }, [])
 
   /** 좌측 검색리스트 관련 상태 */
+  const [searchInput, setSearchInput] = useState<string>('')
+  const [isComposing, setIsComposing] = useState<boolean>(false)
   const [placeList, setPlaceList] = useState<placeDTO[]>(placeListDummyData)
 
   /** 채팅 관련 상태 */
@@ -251,26 +254,30 @@ const PartPage = (): ReactNode => {
           밥팟
         </Link>
 
-        <div className='flex flex-col items-start justify-start gap-1 pl-8'>
-          <span className='font-dohyeon text-2xl'>선호 음식</span>
-          <span className='text-xss text-rcDarkGray'>* 선호 음식을 눌러 카테부 주변의 맛집을 추천 받아보세요!</span>
-
-          <ul className='mb-2 grid w-full grid-cols-4 grid-rows-2 gap-2'>
-            {MainCategories.map(cat => (
-              <li
-                onClick={() => mainCategoryClickHandler(cat)}
-                className={cn(
-                  cat === category.mainCategory && 'bg-rcKakaoYellow hover:bg-rcKakaoYellowHover',
-                  'flex cursor-pointer items-center justify-center rounded-md border-[0.5px] border-solid border-rcGray px-1 py-2 text-[10px] font-semibold hover:bg-rcKakaoYellow',
-                )}
-                key={cat}
-              >
-                {cat}
-              </li>
-            ))}
-          </ul>
+        <div className='relative my-3 flex w-full flex-col items-start justify-start gap-1 pl-8'>
+          <span className='font-dohyeon text-2xl'>밥팟 음식점</span>
+          <div className='flex w-full items-center justify-start border-b-2 border-solid border-rcKakaoYellow'>
+            <LucideIcon name='Search' />
+            <Input
+              type='text'
+              placeholder='식당 이름을 검색해보세요'
+              className='h-9 w-full rounded-none border-0 py-0 text-sm shadow-none outline-none focus:outline-none focus-visible:ring-0 sm:w-60'
+              value={searchInput}
+              onChange={e => setSearchInput(e.target.value)}
+              // onKeyDown={e => {
+              //   if (e.key === 'Enter' && !isComposing) {
+              //     e.preventDefault()
+              //     refetch()
+              //   }
+              // }}
+              onCompositionStart={() => setIsComposing(true)} // 한글 조합 시작
+              onCompositionEnd={() => setIsComposing(false)} // 한글 조합 끝
+            />
+          </div>
         </div>
-
+        <div className='self-end'>
+          총 <span className='text-rcBlue'>12</span>건
+        </div>
         <div className='my-1 h-[1px] w-full bg-rcLightGray' />
 
         <PlaceList
@@ -288,6 +295,7 @@ const PartPage = (): ReactNode => {
         </span>
 
         {/* 채팅내용 */}
+
         <Chatroom
           category={category}
           updateCategory={updateCategory}
